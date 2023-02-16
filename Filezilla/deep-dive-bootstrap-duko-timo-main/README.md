50e4517ced3d695550da2b6aeaf262bc212c7956
Brotjetimo <123451251+Brotjetimo@users.noreply.github.com>
2023-02-16 09:17:09 +0100

    Add files via upload

---
+++ Filezilla/deep-dive-bootstrap-duko-timo-main/README.md
@@ -0,0 +1,5 @@
+# deep-dive-bootstrap-duko-timo
+deep-dive-bootstrap-duko-timo created by GitHub Classroom
+Trellobord: https://trello.com/invite/b/t2UUR60Y/ATTI15325efc39783a2dd875e55c77d8ad29B8C6BE0D/politike-partij
+
+Figma designs: https://www.figma.com/file/VwYIm8FEk7LPytJLVkKLHI/Untitled?node-id=0%3A1&t=6LD7OQJFtm4EZx4w-1
---
+++ Filezilla/deep-dive-bootstrap-duko-timo-main/navbar.php
@@ -0,0 +1,22 @@
+<ul class="nav nav-pills ">
+    <li role="presentation" <?php
+                                    if ($page == 1) {
+                                        echo 'class="active"';
+                                    }
+                                    ?>><a href="index.html">Home</a></li>
+    <li role="presentation" <?php
+                                    if ($page == 2) {
+                                        echo 'class="active"';
+                                    }
+                                    ?>><a href="standpunten-korte-info.html">standpunten</a></li>
+    <li role="presentation" <?php
+                                    if ($page == 3) {
+                                        echo 'class="active"';
+                                    }
+                                    ?>><a href="onze-missie.html">Ons doel</a></li>
+    <li role="presentation" <?php
+                                    if ($page == 4) {
+                                        echo 'class="active"';
+                                    }
+                                    ?>><a href="extra.html">extra</a></li>
+</ul>
